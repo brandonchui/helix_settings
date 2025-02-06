ad4ef34a68052f9cd24de35f7c0c873566b9d075
brandonchui <chuib@usc.edu>
2025-02-06 11:59:15 -0800

    nix2

--- flake.nix
+++ flake.nix
@@ -1,12 +1,15 @@
 {
-  description = "Helix Config";
+  description = "Brandon's Helix configuration";
   
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+  };
 
-  outputs = { self, nixpkgs }: {
-
-    nixosModules.default = { config, pkgs, ... }: {
-      xdg.configFile."helix" = {
+  outputs = { self, nixpkgs, home-manager }: {
+    homeManagerModules.default = { config, ... }: {
+      home.file.".config/helix" = {
         source = ./config;
         recursive = true;
       };
